1dd12cd9af82797adbe3267ef210b05717a6f45b
Craverush12 <arjungujar1738@gmail.com>
2024-01-28 19:41:24 +0530

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,118 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnelem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * toggle navbar
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnelem(navToggler, 'click', toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnelem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active on scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+const activeHeader = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+addEventOnelem(window, "scroll", activeHeader);
+
+
+
+/**
+ * filter tab
+ */
+
+const tabCard = document.querySelectorAll("[data-tab-card]");
+
+let lastTabCard = tabCard[0];
+
+const navigateTab = function () {
+  lastTabCard.classList.remove("active");
+  this.classList.add("active");
+  lastTabCard = this;
+}
+
+addEventOnelem(tabCard, "click", navigateTab);
+
+
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const galleryItems = document.querySelectorAll('.gallery-item img');
+
+  galleryItems.forEach(img => {
+    img.onload = function() {
+      // Set the padding-top to maintain the aspect ratio
+      let ratio = (img.naturalHeight / img.naturalWidth) * 100;
+      img.parentElement.style.paddingTop = ratio + '%';
+      img.parentElement.style.height = 'auto'; // Reset any previously set height
+    };
+  });
+});
